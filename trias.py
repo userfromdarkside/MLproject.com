51ad2c524f7bd23df559a0803159f1c4e031c49e
Minh Tien Phan <tienphan29403@gmail.com>
2024-03-09 13:19:28 +1100

    Update trias.py

--- trias.py
+++ trias.py
@@ -10,8 +10,14 @@ x_train = np.array([
 ])
 y_train = np.array([460, 232, 178])
 
-
-
+# feature scaling
+def zscore_normalize_features(x):
+        mu = np.mean(x,axis=0)
+        sigma = np.std(x,axis=0)
+        x_norm = (x-mu)/sigma
+        return x_norm, sigma, mu
+x_norm, sigma, mu = zscore_normalize_features(x_train)
+m,n = x_norm.shape 
 
 def compute_cost(x,y,w,b):
     m = x.shape[0]
@@ -49,21 +55,21 @@ def gradient_descent(x,y,w_in,b_in,cost_function,gradient_function,alpha,num_ite
         if i<83000002:      # prevent resource exhaustion 
             j_history.append( cost_function(x, y, w, b))
         # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(num_iters / 10000000) == 0:
-            print(f"Iteration {i:4d}: Cost {j_history[-1]:8.2f}   ")
+        #if i% math.ceil(num_iters / 10000000) == 0:
+         #   print(f"Iteration {i:4d}: Cost {j_history[-1]:8.2f}   ")
         
     return w, b, j_history
 
-m,n = x_train.shape 
+
 initial_w = np.zeros(4)
 initial_b = 0.
-iterations = 83000000
+iterations = 10000000
 alpha = 5.0e-7
-w_final, b_final, J_hist = gradient_descent(x_train, y_train, initial_w, initial_b,
+w_final, b_final, J_hist = gradient_descent(x_norm, y_train, initial_w, initial_b,
                                                     compute_cost, compute_gradient, 
                                                     alpha, iterations)
 print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
-# feature scaling
+
 
 
 
