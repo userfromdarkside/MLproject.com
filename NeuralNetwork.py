e1fd28de69f32b591123bb96776ac3930e4cd9c2
Minh Tien Phan <tienphan29403@gmail.com>
2024-03-18 21:50:08 +1100

    Merge branch 'main' of https://github.com/userfromdarkside/MLproject.com

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,3 @@
-# Neural network for binary classification by using Keras
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
@@ -10,10 +9,5 @@ from autils import *
 import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
-# X,y = load_data()
-print(f'The shape of X is: {X.shape}')
-print(f'The shape of y is: {y.shape}')
-# visualize the data
-# ---
 
 
