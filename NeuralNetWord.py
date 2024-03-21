e116a0030a10474e4f635beadbd55ea39b4275f9
Minh Tien Phan <tienphan29403@gmail.com>
2024-03-22 10:39:20 +1100

    basic structure of Neural NetWork

--- NeuralNetWord.py
+++
@@ -1,4 +0,0 @@
-import os
-
-tf_enable_onednn_opts = os.getenv("TF_ENABLE_ONEDNN_OPTS")
-print(tf_enable_onednn_opts)
---
+++ NeuralNetWork.py
@@ -0,0 +1,213 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+from autils import *
+# %matplotlib inline - use to run in jupyter
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+# load dataset
+X, y = load_data()
+# view the variables
+print ('The first element of y is: ', y[0,0])
+print ('The last element of y is: ', y[-1,0])
+# check the dimensions
+print ('The shape of X is: ' + str(X.shape))
+print ('The shape of y is: ' + str(y.shape))
+# visualize subset of data
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+m, n = X.shape
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1)
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    # Display the label above the image
+    ax.set_title(y[random_index,0])
+    ax.set_axis_off()
+""" -the images are of size  20*20, this gives us  400 inputs
+    -a neural network with  25 units in layer 1,  15 units in layer 2 and  1 output unit in layer 3
+    -Therefore, the shapes of W, and b, are
+layer1: The shape of W1 is (400, 25) and the shape of b1 is (25,)
+layer2: The shape of W2 is (25, 15) and the shape of b2 is: (15,)
+layer3: The shape of W3 is (15, 1) and the shape of b3 is: (1,) """
+# Below, using Keras Sequential model and Dense Layer with a sigmoid activation to construct the network described above.
+model = Sequential(
+    [               
+        tf.keras.Input(shape=(400,)),    #specify input size
+        ### START CODE HERE ### 
+        Dense(units=25,activation='sigmoid'),
+        Dense(units=15,activation='sigmoid'),
+        Dense(units=1,activation='sigmoid')
+        
+    ], name = "my_model" 
+)                            
+# The parameter counts shown in the summary correspond to the number of elements in the weight and bias arrays as shown below.
+L1_num_params = 400 * 25 + 25  # W1 parameters  + b1 parameters
+L2_num_params = 25 * 15 + 15   # W2 parameters  + b2 parameters
+L3_num_params = 15 * 1 + 1     # W3 parameters  + b3 parameters
+print("L1 params = ", L1_num_params, ", L2 params = ", L2_num_params, ",  L3 params = ", L3_num_params )
+# We can examine details of the model by first extracting the layers with model.layers and then extracting.. 
+#..the weights with layerx.get_weights() as shown below.
+[layer1, layer2, layer3] = model.layers
+#### Examine Weights shapes
+W1,b1 = layer1.get_weights()
+W2,b2 = layer2.get_weights()
+W3,b3 = layer3.get_weights()
+print(f"W1 shape = {W1.shape}, b1 shape = {b1.shape}")
+print(f"W2 shape = {W2.shape}, b2 shape = {b2.shape}")
+print(f"W3 shape = {W3.shape}, b3 shape = {b3.shape}")
+# The following code will define a loss function and run gradient descent to fit the weights of the model to..
+#..the training data. This will be explained in more detail in the following week.
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    optimizer=tf.keras.optimizers.Adam(0.001),
+)
+model.fit(
+    X,y,
+    epochs=20
+)
+# To run the model on an example to make a prediction, use Keras predict. The..
+#..input to predict is an array so the single example is reshaped to be two dimensional.
+prediction = model.predict(X[0].reshape(1,400))  # a zero
+print(f" predicting a zero: {prediction}")
+prediction = model.predict(X[500].reshape(1,400))  # a one
+print(f" predicting a one:  {prediction}")
+# The output of the model is interpreted as a probability. In the first example above, the..
+#..input is a zero. The model predicts the probability that the input is a one is nearly zero. In..
+#..the second example, the input is a one. The model predicts the probability that the input is..
+#..a one is nearly one. As in the case of logistic regression, the probability is compared to a threshold to make a final prediction.
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print(f"prediction after threshold: {yhat}")
+# Then compare the predictions vs the labels for a random sample of 64 digits which are plotted above
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1,rect=[0, 0.03, 1, 0.92]) #[left, bottom, right, top]
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Predict using the Neural Network
+    prediction = model.predict(X[random_index].reshape(1,400))
+    if prediction >= 0.5:
+        yhat = 1
+    else:
+        yhat = 0
+    
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]},{yhat}")
+    ax.set_axis_off()
+fig.suptitle("Label, yhat", fontsize=16)
+plt.show()
+# Below, build a dense layer subroutine. The example in lecture utilized a for loop to..
+#..visit each unit (j) in the layer and perform the dot product of the..
+#..weights for that unit (W[:,j]) and sum the bias for the unit (b[j]) to form z. An activation function g(z)..
+#..is then applied to that result. This section will not utilize some of the matrix operations..
+#..described in the optional lectures. These will be explored in a later section.
+def my_dense(a_in, W, b, g):
+    """
+    Computes dense layer
+    Args:
+      a_in (ndarray (n, )) : Data, 1 example 
+      W    (ndarray (n,j)) : Weight matrix, n features per unit, j units
+      b    (ndarray (j, )) : bias vector, j units  
+      g    activation function (e.g. sigmoid, relu..)
+    Returns
+      a_out (ndarray (j,))  : j units
+    """
+    units = W.shape[1]
+    a_out = np.zeros(units)
+    for j in range(units):
+        w = W[:,j]
+        z = np.dot(w, a_in) + b[j]
+        a_out[j] = g(z)
+       
+    return(a_out)
+# Quick Check
+x_tst = 0.1*np.arange(1,3,1).reshape(2,)  # (1 examples, 2 features)
+W_tst = 0.1*np.arange(1,7,1).reshape(2,3) # (2 input features, 3 output features)
+b_tst = 0.1*np.arange(1,4,1).reshape(3,)  # (3 features)
+A_tst = my_dense(x_tst, W_tst, b_tst, sigmoid)
+print(A_tst)
+# The following cell builds a three-layer neural network utilizing the my_dense subroutine above.
+def my_sequential(x, W1, b1, W2, b2, W3, b3):
+    a1 = my_dense(x,  W1, b1, sigmoid)
+    a2 = my_dense(a1, W2, b2, sigmoid)
+    a3 = my_dense(a2, W3, b3, sigmoid)
+    return(a3)
+# We can copy trained weights and biases from Tensorflow.
+W1_tmp,b1_tmp = layer1.get_weights()
+W2_tmp,b2_tmp = layer2.get_weights()
+W3_tmp,b3_tmp = layer3.get_weights()
+# make predictions
+prediction = my_sequential(X[0], W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print( "yhat = ", yhat, " label= ", y[0,0])
+prediction = my_sequential(X[500], W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print( "yhat = ", yhat, " label= ", y[500,0])
+# Run the following cell to see predictions from both the Numpy model and the Tensorflow model. This takes a moment to run.
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1,rect=[0, 0.03, 1, 0.92]) #[left, bottom, right, top]
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+
+    # Predict using the Neural Network implemented in Numpy
+    my_prediction = my_sequential(X[random_index], W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+    my_yhat = int(my_prediction >= 0.5)
+
+    # Predict using the Neural Network implemented in Tensorflow
+    tf_prediction = model.predict(X[random_index].reshape(1,400))
+    tf_yhat = int(tf_prediction >= 0.5)
+    
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]},{tf_yhat},{my_yhat}")
+    ax.set_axis_off() 
+fig.suptitle("Label, yhat Tensorflow, yhat Numpy", fontsize=16)
+plt.show()
+
