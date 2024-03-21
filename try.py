1ac0af770102f98c0d47f8b421de2fb87306908a
Minh Tien Phan <tienphan29403@gmail.com>
2024-03-22 09:09:23 +1100

    do something

---
+++ try.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+from autils import *
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+print('hello')
